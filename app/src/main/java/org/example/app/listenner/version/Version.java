71a9c7c60063e7233ca1706829e84e19a4fafa0b
kurages <kurages@kurages.dev>
2024-10-26 01:20:23 +0000

    とりあできた

---
+++ app/src/main/java/org/example/app/listenner/version/Version.java
@@ -0,0 +1,19 @@
+package org.example.app.listenner.version;
+
+import lombok.RequiredArgsConstructor;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class Version extends ListenerAdapter {
+    private final VersionDao dao;
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        if("/version".equals(event.getMessage().getContentRaw())) {
+            event.getChannel().sendMessage(dao.getVersion()).queue();
+        }
+    }
+}
