03cfe0cc182b0eb04a6a79e043c45d8c40fb6bd6
gx1285 <gx1285.git@outlook.jp>
2025-02-03 12:22:27 +0900

    interaction表面

--- app/src/main/java/io/github/pc_com/app/application/insufficientQuestion/InsufficientQuestion.java
+++ app/src/main/java/io/github/pc_com/app/application/insufficientQuestion/InsufficientQuestion.java
@@ -12,8 +12,13 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.channel.ChannelType;
 import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.channel.ChannelCreateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.components.ActionRow;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import net.dv8tion.jda.api.interactions.components.selections.SelectOption;
+import net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu;
 import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
 
 @Slf4j
@@ -39,9 +44,61 @@ public class InsufficientQuestion extends ListenerAdapter {
     private void sendMessage(ThreadChannel thread) {
         var messageData = new MessageCreateBuilder()
                 .addContent(String.format("<@%s> **質問する際のガイドラインは確認しましたか？**\n%s", thread.getOwnerId(), guideLineUrl))
-                .addEmbeds(getEmbeds())
+                .addEmbeds(getEmbeds()).addComponents(getComponents(thread.getOwnerId()))
                 .build();
         thread.sendMessage(messageData).queue();
+    }
+
+    private ActionRow[] getComponents(String threadOwnerId) {
+        ActionRow[] components = {
+                ActionRow.of(
+                        StringSelectMenu
+                                .create(String.format("question_insufficient_all_%s", threadOwnerId))
+                                .setPlaceholder("全質問共通から不足部を選択")
+                                .setMinValues(1)
+                                .setMaxValues(8)
+                                .addOptions(
+                                        SelectOption.of("タイトル", "title"),
+                                        SelectOption.of("マルチポスト", "multipost"),
+                                        SelectOption.of("複数質問", "multiple"),
+                                        SelectOption.of("目的 or 現状", "purpose"),
+                                        SelectOption.of("OS or 型番", "os_model"),
+                                        SelectOption.of("分かっていること不足", "know"),
+                                        SelectOption.of("実践したこと不足", "practice"),
+                                        SelectOption.of("検索不足", "search"))
+                                .build()),
+                ActionRow.of(
+                        StringSelectMenu
+                                .create(String.format("question_insufficient_software_%s", threadOwnerId))
+                                .setPlaceholder("ソフトウェア系から不足部を選択")
+                                .setMinValues(1)
+                                .setMaxValues(5)
+                                .addOptions(
+                                        SelectOption.of("コード不足", "code"),
+                                        SelectOption.of("エラー不足", "error"),
+                                        SelectOption.of("コードブロック不足", "codeblock"),
+                                        SelectOption.of("OS名 or バージョン", "os"),
+                                        SelectOption.of("ライブラリ名 or バージョン", "library"))
+                                .build()),
+                ActionRow.of(
+                        StringSelectMenu
+                                .create(String.format("question_insufficient_hardware_%s", threadOwnerId))
+                                .setPlaceholder("ハードウェア系から不足部を選択")
+                                .setMinValues(1)
+                                .setMaxValues(4)
+                                .addOptions(
+                                        SelectOption.of("PC詳細", "pc_details"),
+                                        SelectOption.of("OS不足", "os"),
+                                        SelectOption.of("型番不足", "model"),
+                                        SelectOption.of("詳しい症状", "symptoms"))
+                                .build()),
+                ActionRow.of(
+                        Button
+                                .secondary(String.format("guideline_ok_%s", threadOwnerId), "GuideLine OK!")
+                                .withEmoji(Emoji.fromUnicode("✅")))
+
+        };
+        return components;
     }
 
     private List<MessageEmbed> getEmbeds() {
